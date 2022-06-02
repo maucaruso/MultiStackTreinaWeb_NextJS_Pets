3448bbb2ebf25e2c6b151c26520f1b303dd9d17e
maucaruso <carusojr@outlook.com>
2022-06-02 08:13:39 -0300

    Finishing first lesson

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import List from "../ui/components/List";
 import Title from "../ui/components/Title";
 
 const Home: NextPage = () => {
@@ -12,6 +13,23 @@ const Home: NextPage = () => {
             pode <strong>adotar um pet virtualmente</strong>
           </span>
         }
+      />
+
+      <List
+        pets={[
+          {
+            id: 1,
+            name: 'Romena',
+            history: 'Lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et.',
+            photo: '/images/romena.jpeg'
+          },
+          {
+            id: 2,
+            name: 'Paradinha',
+            history: 'Lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et.',
+            photo: '/images/romena.jpeg'
+          }
+        ]}
       />
     </div>
   );
---
+++ src/ui/components/List/index.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@mui/material";
+import { Pet } from "../../../data/@types/Pet";
+import {
+  ListStyled,
+  ItemList,
+  Photo,
+  Details,
+  Name,
+  Description,
+} from "./style";
+import { TextService } from '../../../data/services/TextService';
+
+interface ListProps {
+  pets: Pet[];
+}
+
+export default function List(props: ListProps) {
+  const maxTextSize = 200;
+
+  return (
+    <ListStyled>
+      {props.pets.map((pet) => (
+        <ItemList key={pet.id}>
+          <Photo src={pet.photo} alt={pet.name} />
+          <Details>
+            <Name>{pet.name}</Name>
+            <Description>
+              {TextService.limitText(pet.history, maxTextSize)}
+            </Description>
+            <Button variant={"contained"} fullWidth>
+              Adotar {pet.name}
+            </Button>
+          </Details>
+        </ItemList>
+      ))}
+    </ListStyled>
+  );
+}
